95553045d16c4a3d6e84f6f4480c44e4cf758508
EvaIndriana <evaindrianaj45@gmail.com>
2022-03-14 16:06:39 +0700

    tugas 6 ES6

--- Tugas-6-ES6/index.js/index.js
+++ Tugas-6-ES6/index.js/index.js
@@ -1,9 +1,37 @@
-function multiply(a, b = 1) {
-    return a * b;
-  }
-   
-  console.log(multiply(5, 2));
-  // expected output: 10
-   
-  console.log(multiply(5));
-  // expected output: 5 
+//soal 1
+const luas = (alas, tinggi) => {
+      const luas_segitiga = 0.5 * alas * tinggi;
+      return `alas: ${alas} \n tinggi : ${tinggi} \n luas segitiga : ${luas_segitiga}`
+}
+
+const alas = 10;
+const tinggi = 10;
+console.log(luas(alas, tinggi));
+
+console.log("==============================");
+
+//soal 2
+//soal 3
+
+//const newObject = {
+  //firstName: "Muhammad",
+  //lastName: "Iqbal Mubarok",
+  //address: "Jalan Ranamanyar",
+  //hobby: "playing football",
+//}
+
+
+// object
+var studentName = {
+  firstName: 'Muhaamd Iqbal',
+  lastName: 'Iqbal Mubaroq',
+  address: 'Jalan Ranamanyar',
+  hobby: 'playing football'
+};
+
+const {firstName, lastName, address,hobby} = studentName
+
+console.log(firstName, lastName, address,hobby);
+
+
+  
