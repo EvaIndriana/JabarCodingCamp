622982f575d7790a18d28b6b8f84549090656908
EvaIndriana <evaindrianaj45@gmail.com>
2022-03-14 17:20:40 +0700

    tugas-6-ES6

--- Tugas-6-ES6/index.js/index.js
+++ Tugas-6-ES6/index.js/index.js
@@ -3,23 +3,17 @@ const luas = (alas, tinggi) => {
       const luas_segitiga = 0.5 * alas * tinggi;
       return `alas: ${alas} \n tinggi : ${tinggi} \n luas segitiga : ${luas_segitiga}`
 }
-
 const alas = 10;
 const tinggi = 10;
 console.log(luas(alas, tinggi));
-
+console.log("\n")
 console.log("==============================");
 
-//soal 2
-//soal 3
 
-//const newObject = {
-  //firstName: "Muhammad",
-  //lastName: "Iqbal Mubarok",
-  //address: "Jalan Ranamanyar",
-  //hobby: "playing football",
-//}
-// object
+//soal 2
+
+
+//soal 3
 var studentName = {
   firstName: 'Muhaamd',
   lastName: 'Iqbal Mubaroq',
@@ -38,3 +32,13 @@ const east = ["Gill", "Brian", "Noel", "Maggie"]
 
 const combined = [...west, ...east]
 console.log(combined);
+
+console.log("\n")
+console.log("==============================");
+
+//soal 5
+const planet = "earth";
+const view = "glass"
+const hasil = `lorem ${view} dolor sit amet consectetur adipiscing elit ${planet}`;
+console.log(hasil);
+
