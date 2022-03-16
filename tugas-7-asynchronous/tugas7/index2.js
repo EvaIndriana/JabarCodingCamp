df226fca4d6f637def4c723454b91398c1506aaf
EvaIndriana <evaindrianaj45@gmail.com>
2022-03-16 08:51:09 +0700

    tugas-7-asynchronous

---
+++ tugas-7-asynchronous/tugas7/index2.js
@@ -0,0 +1,12 @@
+var readBooksPromise = require('./promise.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+
+  books.forEach(element => readBooksPromise(10000, element, (callbackFn) => {
+    console.log(callbackFn)
+}))
